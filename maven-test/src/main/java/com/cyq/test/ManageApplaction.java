0d2f87ddce71160b0a8ac1e4d57181769079f4d3
lenovo-PC\lenovo <544303626@qq.com>
2017-12-07 09:19:26 +0800

    ss

---
+++ maven-test/src/main/java/com/cyq/test/ManageApplaction.java
@@ -0,0 +1,15 @@
+package com.cyq.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageApplaction 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ManageApplaction.class, args);
+    }
+}
+
+
